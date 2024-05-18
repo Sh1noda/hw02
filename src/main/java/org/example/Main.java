5defeea6098dcd22c4e08e6ebcffb0d48c134a01
iLya Novikov <novikov.redstar@yandex.ru>
2024-05-18 21:05:32 +0300

    city

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello City!");
+        System.out.println("Hello Mitino!");
     }
 }
